90ab2a211cae6af9a7b05f6194af6a3d7af8b5c3
macbook <macbook@macbookpro.lan>
2025-10-16 09:56:39 -0700

    Test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3001;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.end('Hello World from Tamir!\n');
 });
 
 server.listen(port, hostname, () => {
